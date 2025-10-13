27a92368522aac57deb58b591474a5b6dc42c52d
Anna <cameaann@gmail.com>
2025-10-13 19:08:51 +0300

    toDoForm

---
+++ todoApp-frontMentor/src/components/todoForm.tsx
@@ -0,0 +1,37 @@
+import { useContext, useState, type ChangeEvent, type KeyboardEvent } from "react";
+import { type ToDo } from "./ToDoItem";
+import ThemeContext from "../ThemeContext";
+
+type ToDoFormProps = {
+  addItem: (item: ToDo) => void;
+};
+
+const ToDoForm = ({ addItem }: ToDoFormProps) => {
+  const theme = useContext(ThemeContext)
+  const [toDo, setToDo] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setToDo(value);
+  };
+  const handleSubmit = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && toDo.trim() !== "") {
+      const item: ToDo = { id: Date.now(), text: toDo, status: "pending" };
+      addItem(item);
+      setToDo("");
+    }
+  };
+  return (
+    <form>
+      <input
+        className={"todoInput " + theme}
+        value={toDo}
+        onChange={handleChange}
+        onKeyDown={handleSubmit}
+        placeholder="Create a new todo"
+      />
+    </form>
+  );
+};
+
+export default ToDoForm;
