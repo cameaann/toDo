de0d10fd9f04cef1193d78608b33aab5c8d1d245
Anna <cameaann@gmail.com>
2025-10-24 15:49:04 +0300

    toDoItemContent

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,10 +1,9 @@
-import { useContext, useState, useRef, useEffect } from "react";
+import { useContext } from "react";
 import ThemeContext from "../ThemeContext";
 import Cross from "../assets/icon-cross.svg";
 import { type TTask } from "../utils";
 import { useDraggable } from "@dnd-kit/core";
-import { useDebounce } from "../useDebounce";
-
+import ToDoItemContent from "./ToDoItemContent";
 
 type ToDoItemProps = {
   toDo: TTask;
@@ -20,9 +19,6 @@ const ToDoItem = ({
   saveChanges,
 }: ToDoItemProps) => {
   const theme = useContext(ThemeContext);
-  const [editMode, setEditMode] = useState<boolean>(false);
-  const [editedText, setEditedText] = useState<string>(toDo.text);
-  const ref = useRef<HTMLTextAreaElement>(null);
 
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: toDo.id,
@@ -32,39 +28,6 @@ const ToDoItem = ({
         transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
       }
     : undefined;
-
-  const setEdit = (mode: boolean, text: string) => {
-    setEditMode(mode);
-    setEditedText(text);
-  };
-
-  const unsetEditMode = () => {
-    console.log("OnBlur");
-    setEditMode(false);
-    setEditedText("");
-  }
-
-  const debounceSave = useDebounce(() => {
-    toDo.text = editedText;
-    saveChanges(toDo);
-  }, 500);
-
-  const editToDo = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const { value } = e.target;
-    setEditedText(value);
-    debounceSave();
-  };
-
-  const handleInput = () => {
-    if (ref.current) {
-      ref.current.style.height = "auto";
-      ref.current.style.height = `${ref.current.scrollHeight}px`;
-    }
-  };
-
-    useEffect(() => {
-    if (editMode) handleInput();
-  }, [editMode]);
 
   return (
     <>
@@ -83,29 +46,7 @@ const ToDoItem = ({
             checked={toDo.status === "done"}
             onChange={() => toggleStatus(toDo.id)}
           />
-          <div className="content">
-            {editMode ? (
-              <textarea
-                id={toDo.id}
-                ref={ref}
-                className={
-                  toDo.status === "done"
-                    ? "edit-input completed " + theme
-                    : "edit-input " + theme
-                }
-                value={editedText}
-                onChange={(e) => editToDo(e)}
-                onBlur={() => unsetEditMode()}
-              ></textarea>
-            ) : (
-              <label
-                className={toDo.status === "done" ? "completed" : ""}
-                onClick={() => setEdit(true, toDo.text)}
-              >
-                {toDo.text}
-              </label>
-            )}
-          </div>
+          <ToDoItemContent toDo={toDo} saveChanges={saveChanges} />
         </div>
 
         <button
